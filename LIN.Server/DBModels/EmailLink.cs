f000b641bcee89c585d3391af1340301cc72694d
Alex <giraldojhong4@gmail.com>
2023-08-08 00:49:53 -0500

    CSProj

--- LIN.Server/DBModels/EmailLink.cs
+++ LIN.Server/DBModels/EmailLink.cs
@@ -4,13 +4,9 @@
 public class EmailLink
 {
     public int ID { get; set; }
-
     public string Key { get; set; } = string.Empty;
-
     public int Email { get; set; }
-
     public ChangeLinkStatus Status { get; set; }
-
     public DateTime Vencimiento { get; set; }
 
 }
