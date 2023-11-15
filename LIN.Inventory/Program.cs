e6ab9a8bf051f9aaa37707238916fb66cb322cd5
Alex <giraldojhong4@gmail.com>
2023-11-15 16:00:11 -0500

    Usings.cs

--- LIN.Inventory/Program.cs
+++ LIN.Inventory/Program.cs
@@ -1,24 +1,3 @@
-global using LIN.Inventory;
-global using LIN.Inventory.Controllers;
-global using LIN.Inventory.DBModels;
-global using LIN.Modules;
-global using LIN.Inventory.Hubs;
-global using LIN.Inventory.Services;
-global using LIN.Types.Enumerations;
-global using LIN.Types.Inventory.Models;
-global using LIN.Types.Responses;
-global using Http.ResponsesList;
-global using Microsoft.AspNetCore.Mvc;
-global using LIN.Types.Inventory.Enumerations;
-global using Microsoft.AspNetCore.SignalR;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.IdentityModel.Tokens;
-global using Newtonsoft.Json;
-global using LIN.Types.Auth.Models;
-global using LIN.Types.Auth.Abstracts;
-global using LIN.Types.Auth.Enumerations;
-global using System.Diagnostics;
-global using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using LIN.Inventory.Data;
 
