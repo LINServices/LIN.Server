3385507e6c1892d2c985af3a027844465aa2e964
Alex <giraldojhong4@gmail.com>
2024-03-27 23:18:37 -0500

    Comentarios en Outflows.cs

---
+++ LIN.Inventory/Data/Outflows.cs
@@ -0,0 +1,95 @@
+﻿namespace LIN.Inventory.Data;
+
+
+public partial class Outflows
+{
+
+
+    /// <summary>
+    /// Crear nueva salida.
+    /// </summary>
+    /// <param name="data">Modelo.</param>
+    public async static Task<CreateResponse> Create(OutflowDataModel data)
+    {
+        // Obtiene la conexión
+        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
+
+        var res = await Create(data, context);
+        context.CloseActions(connectionKey);
+        return res;
+    }
+
+
+
+    /// <summary>
+    /// Obtiene una salida.
+    /// </summary>
+    /// <param name="id">Id de la salida.</param>
+    /// <param name="includeDetails">Incluir detalles.</param>
+    public async static Task<ReadOneResponse<OutflowDataModel>> Read(int id, bool includeDetails = false)
+    {
+
+        // Obtiene la conexión
+        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
+
+        var res = await Read(id, includeDetails, context);
+        context.CloseActions(connectionKey);
+        return res;
+    }
+
+
+
+    /// <summary>
+    /// Obtiene la lista de salidas asociadas a un inventario.
+    /// </summary>
+    /// <param name="id">Id del inventario.</param>
+    public async static Task<ReadAllResponse<OutflowDataModel>> ReadAll(int id)
+    {
+
+        // Obtiene la conexión
+        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
+        var res = await ReadAll(id, context);
+        context.CloseActions(connectionKey);
+        return res;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// Obtiene las ventas realizadas por un usuario en todos los inventarios
+    /// </summary>
+    public async static Task<ReadOneResponse<int>> VentasOf(int id, int days)
+    {
+
+        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
+        var res = await VentasOf(id, days, context);
+        context.CloseActions(connectionKey);
+        return res;
+
+    }
+
+    public async static Task<ReadAllResponse<SalesModel>> Ventas(int id, int days)
+    {
+
+        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
+        var res = await Ventas(id, days, context);
+        context.CloseActions(connectionKey);
+        return res;
+
+    }
+
+
+
+}
--- LIN.Inventory/Data/Outflows.cs.cs
+++ LIN.Inventory/Data/Outflows.cs.cs
@@ -1,100 +1,15 @@
 ﻿namespace LIN.Inventory.Data;
 
 
-public class Outflows
+public partial class Outflows
 {
 
 
-
-    #region Abstracciones
-
-
     /// <summary>
-    /// Crea una salida de inventario
+    /// Crea una salida de inventario.
     /// </summary>
-    /// <param name="data">Modelo de la salida</param>
-    public async static Task<CreateResponse> Create(OutflowDataModel data)
-    {
-        // Obtiene la conexión
-        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
-
-        var res = await Create(data, context);
-        context.CloseActions(connectionKey);
-        return res;
-    }
-
-
-
-    /// <summary>
-    /// Obtiene una salida
-    /// </summary>
-    /// <param name="id">Id de la salida</param>
-    public async static Task<ReadOneResponse<OutflowDataModel>> Read(int id, bool includeDetails = false)
-    {
-
-        // Obtiene la conexión
-        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
-
-        var res = await Read(id, includeDetails, context);
-        context.CloseActions(connectionKey);
-        return res;
-    }
-
-
-
-    /// <summary>
-    /// Obtiene la lista de salidas asociadas a un inventario
-    /// </summary>
-    /// <param name="id">Id del inventario</param>
-    public async static Task<ReadAllResponse<OutflowDataModel>> ReadAll(int id)
-    {
-
-        // Obtiene la conexión
-        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
-        var res = await ReadAll(id, context);
-        context.CloseActions(connectionKey);
-        return res;
-    }
-
-
-
-    /// <summary>
-    /// Obtiene las ventas realizadas por un usuario en todos los inventarios
-    /// </summary>
-    public async static Task<ReadOneResponse<int>> VentasOf(int id, int days)
-    {
-
-        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
-        var res = await VentasOf(id, days, context);
-        context.CloseActions(connectionKey);
-        return res;
-
-    }
-
-
-
-
-    public async static Task<ReadAllResponse<SalesModel>> Ventas(int id, int days)
-    {
-
-        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
-        var res = await Ventas(id, days, context);
-        context.CloseActions(connectionKey);
-        return res;
-
-    }
-
-
-
-    #endregion
-
-
-
-    /// <summary>
-    /// Crea una salida de inventario
-    /// </summary>
-    /// <param name="data">Modelo de la salida</param>
-    /// <param name="context">Contexto de conexión</param>
+    /// <param name="data">Modelo de la salida.</param>
+    /// <param name="context">Contexto de conexión.</param>
     public async static Task<CreateResponse> Create(OutflowDataModel data, Conexión context)
     {
 
@@ -184,10 +99,11 @@ public class Outflows
 
 
     /// <summary>
-    /// Obtiene una salida
+    /// Obtiene una salida.
     /// </summary>
-    /// <param name="id">Id de la salida</param>
-    /// <param name="context">Contexto de conexión</param>
+    /// <param name="id">Id de la salida.</param>
+    /// <param name="includeDetails">Incluir los detalles.</param>
+    /// <param name="context">Contexto de conexión.</param>
     public async static Task<ReadOneResponse<OutflowDataModel>> Read(int id, bool includeDetails, Conexión context)
     {
 
@@ -244,10 +160,10 @@ public class Outflows
 
 
     /// <summary>
-    /// Obtiene la lista de salidas asociadas a un inventario
+    /// Obtiene la lista de salidas asociadas a un inventario.
     /// </summary>
-    /// <param name="id">Id del inventario</param>
-    /// <param name="context">Contexto de conexión</param>
+    /// <param name="id">Id del inventario.</param>
+    /// <param name="context">Contexto de conexión.</param>
     public async static Task<ReadAllResponse<OutflowDataModel>> ReadAll(int id, Conexión context)
     {
 
@@ -282,6 +198,20 @@ public class Outflows
         }
         return new();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -375,13 +305,6 @@ public class Outflows
 
         return new();
     }
-
-
-
-
-
-
-
 
     /// <summary>
     /// Obtiene las ventas realizadas por un usuario en todos los inventarios
