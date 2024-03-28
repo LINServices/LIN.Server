2f258430b0e114b7c42f8e88f0ff6912af99121a
Alex <giraldojhong4@gmail.com>
2024-03-27 22:47:47 -0500

    Comentarios en Inventaries.cs

--- LIN.Inventory/Data/Inventories.cs
+++ LIN.Inventory/Data/Inventories.cs
@@ -1,17 +1,14 @@
 ﻿namespace LIN.Inventory.Data;
 
 
-public class Inventories
+public partial class Inventories
 {
 
 
-    #region Abstracciones
-
-
     /// <summary>
-    /// Crea un nuevo inventario
+    /// Crear nuevo inventario.
     /// </summary>
-    /// <param name="data">Modelo del inventario</param>
+    /// <param name="data">Modelo.</param>
     public async static Task<CreateResponse> Create(InventoryDataModel data)
     {
 
@@ -26,7 +23,7 @@ public class Inventories
 
 
     /// <summary>
-    /// Obtiene un inventario por medio del Id
+    /// Obtiene un inventario.
     /// </summary>
     /// <param name="id">Id del inventario</param>
     public async static Task<ReadOneResponse<InventoryDataModel>> Read(int id)
@@ -45,9 +42,9 @@ public class Inventories
 
 
     /// <summary>
-    /// Obtiene la lista de inventarios asociados a una cuenta
+    /// Obtiene la lista de inventarios asociados a un perfil.
     /// </summary>
-    /// <param name="id">Id de la cuenta</param>
+    /// <param name="id">Id del perfil.</param>
     public async static Task<ReadAllResponse<InventoryDataModel>> ReadAll(int id)
     {
 
@@ -63,9 +60,9 @@ public class Inventories
 
 
     /// <summary>
-    /// Obtiene la valuación de los inventarios donde un usuario es admin
+    /// Obtiene la valuación de los inventarios donde un usuario es administrador.
     /// </summary>
-    /// <param name="id">Id de la cuenta</param>
+    /// <param name="id">Id del perfil.</param>
     public async static Task<ReadOneResponse<decimal>> ValueOf(int id)
     {
 
@@ -79,12 +76,10 @@ public class Inventories
 
 
 
-
-
-
-
-
-
+    /// <summary>
+    /// Obtener el Id de un inventario.
+    /// </summary>
+    /// <param name="id">Id del producto.</param>
     public async static Task<ReadOneResponse<int>> FindByProduct(int id)
     {
 
@@ -96,6 +91,12 @@ public class Inventories
         return response;
     }
 
+
+
+    /// <summary>
+    /// Obtener el Id de un inventario.
+    /// </summary>
+    /// <param name="id">Id del detalle de producto.</param>
     public async static Task<ReadOneResponse<int>> FindByProductDetail(int id)
     {
 
@@ -109,7 +110,10 @@ public class Inventories
 
 
 
-
+    /// <summary>
+    /// Obtener el id de un inventario.
+    /// </summary>
+    /// <param name="id">Id de la entrada.</param>
     public async static Task<ReadOneResponse<int>> FindByInflow(int id)
     {
 
@@ -123,6 +127,10 @@ public class Inventories
 
 
 
+    /// <summary>
+    /// Obtener el id de un inventario.
+    /// </summary>
+    /// <param name="id">Id de la salida.</param>
     public async static Task<ReadOneResponse<int>> FindByOutflow(int id)
     {
 
@@ -135,317 +143,5 @@ public class Inventories
     }
 
 
-    #endregion
-
-
-
-    /// <summary>
-    /// Crea un nuevo inventario
-    /// </summary>
-    /// <param name="data">Modelo del inventario</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<CreateResponse> Create(InventoryDataModel data, Conexión context)
-    {
-
-        // Modelo
-        data.ID = 0;
-
-        // Transacción
-        using (var transaction = context.DataBase.Database.BeginTransaction())
-        {
-            try
-            {
-
-                // InventoryId
-                context.DataBase.Inventarios.Add(data);
-
-                // Guarda el inventario
-                await context.DataBase.SaveChangesAsync();
-
-
-                // Accesos
-                DateTime dateTime = DateTime.Now;
-                foreach (var acceso in data.UsersAccess)
-                {
-                    // Propiedades
-                    acceso.ID = 0;
-                    acceso.Fecha = dateTime;
-                    acceso.Inventario = data.ID;
-
-                    // Accesos
-                    context.DataBase.AccesoInventarios.Add(acceso);
-
-                }
-
-                // Guarda los cambios
-                await context.DataBase.SaveChangesAsync();
-
-                // Finaliza
-                transaction.Commit();
-                return new(Responses.Success, data.ID);
-            }
-            catch (Exception ex)
-            {
-                transaction.Rollback();
-                context.DataBase.Remove(data);
-                ServerLogger.LogError(ex.Message);
-            }
-        }
-
-
-        return new();
-    }
-
-
-
-    /// <summary>
-    /// Obtiene un inventario
-    /// </summary>
-    /// <param name="id">Id del inventario</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadOneResponse<InventoryDataModel>> Read(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-            var res = await context.DataBase.Inventarios.FirstOrDefaultAsync(T => T.ID == id);
-
-            // Si no existe el modelo
-            if (res == null)
-                return new(Responses.NotExistAccount);
-
-            return new(Responses.Success, res);
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-    }
-
-
-
-    /// <summary>
-    /// Obtiene la lista de inventarios asociados a una cuenta
-    /// </summary>
-    /// <param name="id">Id de la cuenta</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadAllResponse<InventoryDataModel>> ReadAll(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-
-            var res = from AI in context.DataBase.AccesoInventarios
-                      where AI.ProfileID == id && AI.State == InventoryAccessState.Accepted
-                      join I in context.DataBase.Inventarios on AI.Inventario equals I.ID
-                      select new InventoryDataModel()
-                      {
-                          MyRol = AI.Rol,
-                          Creador = I.Creador,
-                          Direction = I.Direction,
-                          ID = I.ID,
-                          Nombre = I.Nombre,
-                          UsersAccess = I.UsersAccess
-                      };
-
-
-            var modelos = await res.ToListAsync();
-
-            if (modelos != null)
-                return new(Responses.Success, modelos);
-
-            return new(Responses.NotRows);
-
-
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-
-
-
-
-    }
-
-
-
-    /// <summary>
-    /// Obtiene la valuación de los inventarios donde un usuario es admin
-    /// </summary>
-    /// <param name="id">Id de la cuenta</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadOneResponse<decimal>> ValueOf(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-
-            var query = from acceso in context.DataBase.AccesoInventarios
-                        where acceso.ProfileID == id
-                        && acceso.State == InventoryAccessState.Accepted
-                        && acceso.Rol == InventoryRoles.Administrator
-
-                        join inventory in context.DataBase.Inventarios
-                        on acceso.Inventario equals inventory.ID
-
-                        join product in context.DataBase.Productos on inventory.ID equals product.InventoryId
-                        join productDetails in context.DataBase.ProductoDetalles on product.Id equals productDetails.ProductId
-                        where productDetails.Estado == ProductStatements.Normal
-                        select productDetails.PrecioVenta * productDetails.Quantity;
-
-
-            var valor = await query.SumAsync();
-
-            // Retorna
-            return new(Responses.Success, valor);
-
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-    }
-
-
-
-
-    /// <summary>
-    /// Obtiene un inventario según el producto,
-    /// </summary>
-    /// <param name="id">Id del producto</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadOneResponse<int>> FindByProduct(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-
-            var res = await (from p in context.DataBase.Productos
-                      where p.Id == id
-                      select p.InventoryId).FirstOrDefaultAsync();
-
-            // Si no existe el modelo
-            if (res == 0)
-                return new(Responses.NotExistAccount);
-
-            return new(Responses.Success, res);
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-    }
-
-
-
-
-    /// <summary>
-    /// Obtiene un inventario según el producto,
-    /// </summary>
-    /// <param name="id">Id del producto detalle</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadOneResponse<int>> FindByProductDetail(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-
-            var res = await (from p in context.DataBase.ProductoDetalles
-                             where p.Id == id
-                             select p.Product.InventoryId).FirstOrDefaultAsync();
-
-            // Si no existe el modelo
-            if (res == 0)
-                return new(Responses.NotExistAccount);
-
-            return new(Responses.Success, res);
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-    }
-
-
-
-
-
-    /// <summary>
-    /// Obtiene un inventario según una entrada,
-    /// </summary>
-    /// <param name="id">Id del entrada</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadOneResponse<int>> FindByInflow(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-
-            var res = await (from p in context.DataBase.Entradas
-                             where p.ID == id
-                             select p.InventoryId).FirstOrDefaultAsync();
-
-            // Si no existe el modelo
-            if (res == 0)
-                return new(Responses.NotExistAccount);
-
-            return new(Responses.Success, res);
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-    }
-
-
-
-    /// <summary>
-    /// Obtiene un inventario según una salida,
-    /// </summary>
-    /// <param name="id">Id del salida</param>
-    /// <param name="context">Contexto de conexión</param>
-    public async static Task<ReadOneResponse<int>> FindByOutflow(int id, Conexión context)
-    {
-
-        // Ejecución
-        try
-        {
-
-            var res = await (from p in context.DataBase.Salidas
-                             where p.ID == id
-                             select p.InventoryId).FirstOrDefaultAsync();
-
-            // Si no existe el modelo
-            if (res == 0)
-                return new(Responses.NotExistAccount);
-
-            return new(Responses.Success, res);
-        }
-        catch (Exception ex)
-        {
-            ServerLogger.LogError(ex.Message);
-        }
-
-        return new();
-    }
 
 }
---
+++ LIN.Inventory/Data/Inventories.cs.cs
@@ -0,0 +1,315 @@
+﻿namespace LIN.Inventory.Data;
+
+
+public partial class Inventories
+{
+
+
+    /// <summary>
+    /// Crea un nuevo inventario.
+    /// </summary>
+    /// <param name="data">Modelo del inventario</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<CreateResponse> Create(InventoryDataModel data, Conexión context)
+    {
+
+        // Modelo
+        data.ID = 0;
+
+        // Transacción
+        using (var transaction = context.DataBase.Database.BeginTransaction())
+        {
+            try
+            {
+
+                // InventoryId
+                context.DataBase.Inventarios.Add(data);
+
+                // Guarda el inventario
+                await context.DataBase.SaveChangesAsync();
+
+
+                // Accesos
+                DateTime dateTime = DateTime.Now;
+                foreach (var acceso in data.UsersAccess)
+                {
+                    // Propiedades
+                    acceso.ID = 0;
+                    acceso.Fecha = dateTime;
+                    acceso.Inventario = data.ID;
+
+                    // Accesos
+                    context.DataBase.AccesoInventarios.Add(acceso);
+
+                }
+
+                // Guarda los cambios
+                await context.DataBase.SaveChangesAsync();
+
+                // Finaliza
+                transaction.Commit();
+                return new(Responses.Success, data.ID);
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                context.DataBase.Remove(data);
+                ServerLogger.LogError(ex.Message);
+            }
+        }
+
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene un inventario.
+    /// </summary>
+    /// <param name="id">Id del inventario</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadOneResponse<InventoryDataModel>> Read(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+            var res = await context.DataBase.Inventarios.FirstOrDefaultAsync(T => T.ID == id);
+
+            // Si no existe el modelo
+            if (res == null)
+                return new(Responses.NotExistAccount);
+
+            return new(Responses.Success, res);
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene la lista de inventarios asociados a un perfil.
+    /// </summary>
+    /// <param name="id">Id del perfil.</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadAllResponse<InventoryDataModel>> ReadAll(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var res = from AI in context.DataBase.AccesoInventarios
+                      where AI.ProfileID == id && AI.State == InventoryAccessState.Accepted
+                      join I in context.DataBase.Inventarios on AI.Inventario equals I.ID
+                      select new InventoryDataModel()
+                      {
+                          MyRol = AI.Rol,
+                          Creador = I.Creador,
+                          Direction = I.Direction,
+                          ID = I.ID,
+                          Nombre = I.Nombre,
+                          UsersAccess = I.UsersAccess
+                      };
+
+
+            var modelos = await res.ToListAsync();
+
+            if (modelos != null)
+                return new(Responses.Success, modelos);
+
+            return new(Responses.NotRows);
+
+
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+
+
+
+
+    }
+
+
+
+    /// <summary>
+    /// Obtiene la valuación de los inventarios donde un usuario es administrador.
+    /// </summary>
+    /// <param name="id">Id del perfil</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadOneResponse<decimal>> ValueOf(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var query = from acceso in context.DataBase.AccesoInventarios
+                        where acceso.ProfileID == id
+                        && acceso.State == InventoryAccessState.Accepted
+                        && acceso.Rol == InventoryRoles.Administrator
+
+                        join inventory in context.DataBase.Inventarios
+                        on acceso.Inventario equals inventory.ID
+
+                        join product in context.DataBase.Productos on inventory.ID equals product.InventoryId
+                        join productDetails in context.DataBase.ProductoDetalles on product.Id equals productDetails.ProductId
+                        where productDetails.Estado == ProductStatements.Normal
+                        select productDetails.PrecioVenta * productDetails.Quantity;
+
+
+            var valor = await query.SumAsync();
+
+            // Retorna
+            return new(Responses.Success, valor);
+
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene un inventario.
+    /// </summary>
+    /// <param name="id">Id del producto</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadOneResponse<int>> FindByProduct(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var res = await (from p in context.DataBase.Productos
+                             where p.Id == id
+                             select p.InventoryId).FirstOrDefaultAsync();
+
+            // Si no existe el modelo
+            if (res == 0)
+                return new(Responses.NotExistAccount);
+
+            return new(Responses.Success, res);
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene un inventario,
+    /// </summary>
+    /// <param name="id">Id del producto detalle</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadOneResponse<int>> FindByProductDetail(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var res = await (from p in context.DataBase.ProductoDetalles
+                             where p.Id == id
+                             select p.Product.InventoryId).FirstOrDefaultAsync();
+
+            // Si no existe el modelo
+            if (res == 0)
+                return new(Responses.NotExistAccount);
+
+            return new(Responses.Success, res);
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene un inventario según una entrada.
+    /// </summary>
+    /// <param name="id">Id del entrada</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadOneResponse<int>> FindByInflow(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var res = await (from p in context.DataBase.Entradas
+                             where p.ID == id
+                             select p.InventoryId).FirstOrDefaultAsync();
+
+            // Si no existe el modelo
+            if (res == 0)
+                return new(Responses.NotExistAccount);
+
+            return new(Responses.Success, res);
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+    }
+
+
+
+    /// <summary>
+    /// Obtiene un inventario según una salida.
+    /// </summary>
+    /// <param name="id">Id del salida</param>
+    /// <param name="context">Contexto de conexión</param>
+    public async static Task<ReadOneResponse<int>> FindByOutflow(int id, Conexión context)
+    {
+
+        // Ejecución
+        try
+        {
+
+            var res = await (from p in context.DataBase.Salidas
+                             where p.ID == id
+                             select p.InventoryId).FirstOrDefaultAsync();
+
+            // Si no existe el modelo
+            if (res == 0)
+                return new(Responses.NotExistAccount);
+
+            return new(Responses.Success, res);
+        }
+        catch (Exception ex)
+        {
+            ServerLogger.LogError(ex.Message);
+        }
+
+        return new();
+    }
+
+
+
+}
